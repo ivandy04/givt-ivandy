ef50c7e71fd9c89cd3e2c424b371a7d4c5439554
ivandy04 <69422570+ivandy04@users.noreply.github.com>
2020-08-11 15:34:59 +0700

    Update ip4.sol

--- ivangivt new/contracts/ip4.sol
+++ ivangivt new/contracts/ip4.sol
@@ -2,22 +2,28 @@ pragma solidity >=0.4.21 <0.7.0;
 
 contract ip4 {
 
-    mapping (address => string) ips;
-
-    constructor() public{
-        ips[msg.sender]= "127.0.0.2";
+    mapping (address => string) public ips;
+    string public ip4Regex="^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]{1,2})(\\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]{1,2})){3}$";
+    constructor() public {
+        ips[msg.sender]="127.0.0.2";
     }
 
-    
-    function set_ip(string memory ip) public {
-        ips[msg.sender] = ip;
+    function set_ip(string memory ip)public  returns (bool){
+        if (keccak256(abi.encodePacked(ip)) == keccak256(abi.encodePacked(ip4Regex))) {
+           ips[msg.sender] = ip;
+            return true;
+        }else{
+            return false;
+        }
+        
     }
 
-    function delete_ip(address add) public {
+    function delete_ip(address add)public {
         delete ips[add];
     }
 
-    function get_ip(address addr) public view returns (string memory)  {
+    function get_ip(address addr)public view returns (string memory) {
         return ips[addr];
     }
+    
 }
